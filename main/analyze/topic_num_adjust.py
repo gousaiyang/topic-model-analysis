5968f9dd3d836ff92b14d2807acc258907a064a0
Saiyang Gou <t-sagou@microsoft.com>
2018-08-22 11:11:39 +0800

    Plot perplexity of different topics

--- main/analyze/topic_num_adjust.py
+++ main/analyze/topic_num_adjust.py
@@ -1,6 +1,9 @@
-from ..model import twitter_lda
-
 import colorlabels as cl
+import matplotlib.pyplot as plt
+
+from util import parse_data_from_log, twlda_result_file
+
+from ..model import LOGFILE, twitter_lda
 
 
 def twlda_multiple_run(min_topics, max_topics, iteration):
@@ -12,5 +15,20 @@ def twlda_multiple_run(min_topics, max_topics, iteration):
                     iteration=iteration)
 
 
-def plot_diff_topics():
-    raise NotImplementedError
+def plot_diff_topics(min_topics, max_topics, regex, encoding='utf-8'):
+    plt.title('Different Number of Topics')
+    plt.xlabel('topics')
+    plt.ylabel('perplexity')
+
+    data_x = []
+    data_y = []
+
+    for topics in range(min_topics, max_topics + 1):
+        logfilename = twlda_result_file('test-%d/%s' % (topics, LOGFILE))
+        perplexity = parse_data_from_log(logfilename, encoding=encoding,
+                                         regex_y=regex, cast_y=float)[1][-1]
+        data_x.append(topics)
+        data_y.append(perplexity)
+
+    plt.plot(data_x, data_y)
+    plt.show()
--- main/model/__init__.py
+++ main/model/__init__.py
@@ -1,2 +1,2 @@
 from .topic_model import lda_topic_model
-from .twitter_lda import twitter_lda
+from .twitter_lda import twitter_lda, LOGFILE
