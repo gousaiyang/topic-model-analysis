152951b6c1f22dcc8bab9d7caec65413b0931086
Saiyang Gou <t-sagou@microsoft.com>
2018-08-29 16:26:04 +0800

    Improvements in visualization
    - Show word/user weights in tooltips
    - Show user information

--- main/analyze/visualization_twlda.py
+++ main/analyze/visualization_twlda.py
@@ -1,12 +1,14 @@
+import ast
 import json
 import re
 import time
 
 import colorlabels as cl
 
-from util import (file_read_contents, file_read_lines, file_write_contents,
-                  open_html_in_browser, re_sub_literal, report_file,
-                  twlda_result_file, twlda_source_file, visual_file)
+from util import (csv_reader, data_source_file, file_read_contents,
+                  file_read_lines, file_write_contents, open_html_in_browser,
+                  re_sub_literal, report_file, twlda_result_file,
+                  twlda_source_file, visual_file)
 
 
 def parse_user_topic(desc, encoding='utf-8'):
@@ -39,12 +41,35 @@ def parse_topic_words(desc, encoding='utf-8'):
     return topic_words
 
 
-def organize_data(desc, user_topic, topic_words, topusers=10):
+def load_user_info(userinfofile):
+    user_info = {}
+
+    for row in csv_reader(userinfofile):
+        row['favourites_count'] = int(row['favourites_count'])
+        row['followers_count'] = int(row['followers_count'])
+        row['friends_count'] = int(row['friends_count'])
+        row['listed_count'] = int(row['listed_count'])
+        row['statuses_count'] = int(row['statuses_count'])
+        row['verified'] = ast.literal_eval(row['verified'])
+        user_info[row['screen_name']] = row
+
+    return user_info
+
+
+def show_topic_words(topic_words, topic_id):
+    sum_prob = sum(p for w, p in topic_words[topic_id])
+    return [{
+        'word': w,
+        'prob': p / sum_prob,
+    } for w, p in topic_words[topic_id]]
+
+
+def organize_data(desc, user_topic, topic_words, user_info, topusers=10):
     num_topics = len(topic_words)
 
     topics = [{
         'id': i,
-        'words': [item[0] for item in topic_words[i]],
+        'words': show_topic_words(topic_words, i),
         'users': []
     } for i in range(num_topics)]
 
@@ -54,7 +79,12 @@ def organize_data(desc, user_topic, topic_words, topusers=10):
         for item in user_topic[:topusers]:
             sourcefilename = twlda_source_file('%s/%s.txt' % (desc, item[0]))
             text = file_read_contents(sourcefilename)
-            topic['users'].append({'username': item[0], 'text': text})
+            topic['users'].append({
+                            'username': item[0],
+                            'weight': item[i + 1],
+                            'info': user_info.get(item[0]) or {},
+                            'text': text
+                        })
 
         topic['weight'] = sum(item[i + 1] for item in user_topic)
 
@@ -70,7 +100,7 @@ def export_html(keyword, desc, data, open_browser):
     data = json.dumps({
         'title': '%s - %s' % (keyword, desc),
         'topics': data,
-        'currentUser': {}
+        'currentUser': {'info': {}}
     })
     template = file_read_contents(visual_file('template.html'))
     data = re_sub_literal(r'var data =(.*)', 'var data = ' + data, template)
@@ -85,10 +115,12 @@ def export_html(keyword, desc, data, open_browser):
         open_html_in_browser(reportfile)
 
 
-def visualization_twlda(keyword, desc, encoding='utf-8', open_browser=True):
+def visualization_twlda(keyword, desc, userinfofile, encoding='utf-8',
+                        open_browser=True):
     cl.section('Twitter-LDA Visualization')
 
     user_topic = parse_user_topic(desc, encoding=encoding)
     topic_words = parse_topic_words(desc, encoding=encoding)
-    result = organize_data('test', user_topic, topic_words)
+    user_info = load_user_info(data_source_file(userinfofile))
+    result = organize_data('test', user_topic, topic_words, user_info)
     export_html(keyword, desc, result, open_browser)
