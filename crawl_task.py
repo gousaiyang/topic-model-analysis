bfc019306b354c267ed8448d0f52dd60b41c41a9
Saiyang Gou <t-sagou@microsoft.com>
2018-09-20 17:31:20 +0800

    Split into two tasks

---
+++ crawl_task.py
@@ -0,0 +1,45 @@
+import datetime
+import os
+
+import colorlabels as cl
+from decouple import config
+
+from main import data_retriever, retweets_recover, user_info_retriever
+from util import TimeMeasure, csv_reader, data_source_file
+
+# Adjust working directory to current script directory (project root).
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+# Set NO_COLOR=True or NO_COLOR=1 to disable color output.
+if config('NO_COLOR', cast=bool, default=False):
+    cl.config(color_span=0)
+
+# Parameters
+DATEBACK = datetime.timedelta(weeks=1)  # The date range to collect data.
+QUERY = '"java"'  # Twitter search query string.
+PROXY = None  # The proxy used to collect Twitter data. (None or 'http://xxx')
+TWSCRAPE_POOLSIZE = 20  # Number of processes for twitterscraper.
+
+
+def get_usernames(tweets_file):
+    return list(set(row['user'] for row in
+                    csv_reader(data_source_file(tweets_file))))
+
+
+def main():
+    tweets_file = 'twdata-java.csv'
+    userinfo_file = 'twusers-java.csv'
+
+    # Retrieve (Scrape + Recover Retweets + Get User Info)
+    data_retriever('twitterscraper', QUERY, tweets_file, lang='en',
+                   proxy=PROXY, remove_duplicates=False,
+                   twscrape_poolsize=TWSCRAPE_POOLSIZE,
+                   twscrape_begindate=datetime.date.today() - DATEBACK)
+    tweets_file_recovered = retweets_recover(tweets_file)
+    usernames = get_usernames(tweets_file_recovered)
+    user_info_retriever(usernames, userinfo_file)
+
+
+if __name__ == '__main__':
+    with TimeMeasure('crawl_task'):
+        main()
--- train_task.py
+++ train_task.py
@@ -1,5 +1,4 @@
 import contextlib
-import datetime
 import os
 import time
 import zipfile
@@ -7,11 +6,9 @@ import zipfile
 import colorlabels as cl
 from decouple import config
 
-from main import (data_retriever, plot_diff_topics, retweets_recover,
-                  text_preprocessor_twlda, twitter_lda, user_info_retriever,
+from main import (plot_diff_topics, text_preprocessor_twlda, twitter_lda,
                   visualization_twlda)
-from util import (TimeMeasure, csv_reader, data_source_file, pipe_encoding,
-                  report_file, retry_until_success)
+from util import TimeMeasure, pipe_encoding, report_file, retry_until_success
 
 # Adjust working directory to current script directory (project root).
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -21,20 +18,11 @@ if config('NO_COLOR', cast=bool, default=False):
     cl.config(color_span=0)
 
 # Parameters
-DATEBACK = datetime.timedelta(weeks=1)  # The date range to collect data.
-QUERY = '"java"'  # Twitter search query string.
 KEYWORD = 'Java'  # Only shown on the final report.
-PROXY = None  # The proxy used to collect Twitter data. (None or 'http://xxx')
-TWSCRAPE_POOLSIZE = 20  # Number of processes for twitterscraper.
 MIN_TOPICS = 6  # Minimum number of topics for training.
 MAX_TOPICS = 30  # Maximum number of topics for training.
 ITERATIONS = 4000  # The number of iterations for training.
 REPORT_ONLY_MINIMA = True  # Whether only to generate reports at minima points.
-
-
-def get_usernames(tweets_file):
-    return list(set(row['user'] for row in
-                    csv_reader(data_source_file(tweets_file))))
 
 
 def compress_report_files(tag, report_files):
@@ -56,18 +44,9 @@ def compress_report_files(tag, report_files):
 
 def main():
     tag = time.strftime('java-%Y%m%d%H%M%S')
-    tweets_file = 'twdata-java.csv'
+    tweets_file_recovered = 'twdata-java-recovered.csv'
     userinfo_file = 'twusers-java.csv'
     num_topics_range = list(range(MIN_TOPICS, MAX_TOPICS + 1))
-
-    # Retrieve (Scrape + Recover Retweets + Get User Info)
-    data_retriever('twitterscraper', QUERY, tweets_file, lang='en',
-                   proxy=PROXY, remove_duplicates=False,
-                   twscrape_poolsize=TWSCRAPE_POOLSIZE,
-                   twscrape_begindate=datetime.date.today() - DATEBACK)
-    tweets_file_recovered = retweets_recover(tweets_file)
-    usernames = get_usernames(tweets_file_recovered)
-    user_info_retriever(usernames, userinfo_file)
 
     # Preprocess
     retry_until_success(text_preprocessor_twlda, tweets_file_recovered[:-4],
@@ -99,5 +78,5 @@ def main():
 
 
 if __name__ == '__main__':
-    with TimeMeasure('task_total'):
+    with TimeMeasure('train_task'):
         main()
