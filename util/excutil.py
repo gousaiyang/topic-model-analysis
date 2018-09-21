9bae2b110c099ca1d8f96d9c2f693cfb4f472535
Saiyang Gou <t-sagou@microsoft.com>
2018-09-21 10:50:20 +0800

    Max retries

--- util/excutil.py
+++ util/excutil.py
@@ -8,6 +8,8 @@ def get_exc_line():
 
 
 def retry_until_success(func, *args, **kwargs):
+    MAX_RETRY = 2
+    retry = 0
     ret = None
 
     while True:
@@ -17,7 +19,13 @@ def retry_until_success(func, *args, **kwargs):
             except KeyboardInterrupt:
                 raise
             except Exception:
-                cl.error('Error: %s\nRetrying...' % traceback.format_exc())
+                if retry >= MAX_RETRY:
+                    cl.error('Error: %sMax retries exceeded, terminating.'
+                             % traceback.format_exc())
+                    raise
+
+                cl.error('Error: %sRetrying...' % traceback.format_exc())
+                retry += 1
             else:
                 return ret
         except KeyboardInterrupt:
